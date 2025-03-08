626f3e88d276ad1fb88ecddb522401b96e7e546f
iKREISHI <vadas25@yandex.ru>
2025-03-08 18:10:52 +0500

    add Makefile

---
+++ backend/src/entrypoint.py
@@ -0,0 +1,28 @@
+import os
+import django
+
+# Установка переменной окружения для конфигурации Django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'base.settings')
+
+# Инициализация приложений Django
+django.setup()
+
+from apps.users.models import User
+
+SUPERUSER_USERNAME = 'root'
+SUPERUSER_PASSWORD = 'root'
+
+try:
+    if not User.objects.filter(username=SUPERUSER_USERNAME).exists():
+        User.objects.create_superuser(
+            username=SUPERUSER_USERNAME,
+            password=SUPERUSER_PASSWORD,
+            first_name='Admin',
+            last_name='Admin',
+            patronymic='Admin',
+        )
+        print(f"Суперпользователь успешно создан.")
+    else:
+        print(f"Суперпользователь уже существует.")
+except Exception as e:
+    print(f"Ошибка при создании суперпользователя: {e}")
