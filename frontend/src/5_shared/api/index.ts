247b376dfae5a9598600d62b10ffdb35db825d65
chmonya <aksarinila15@gmail.com>
2025-03-13 15:37:02 +0500

    init backend openapi

---
+++ frontend/src/5_shared/api/index.ts
@@ -0,0 +1,2 @@
+export {GET, POST, PUT, DELETE} from './client.ts';
+export type { User } from './models';
---
+++ frontend/src/5_shared/api/models.ts
@@ -0,0 +1,4 @@
+import type { components } from "./types.ts";
+
+export type User = components["schemas"]["User"];
+
