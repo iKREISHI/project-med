5a625de130bc6e5fcf4fa2568bb48c6122989998
shiro-kreishi <vadas25@yandex.ru>
2025-03-18 10:03:27 +0500

    added dahpne asgi server

--- backend/src/config/asgi.py
+++ backend/src/config/asgi.py
@@ -14,6 +14,10 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.config')
 
-application = get_asgi_application()
+# application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    # Just HTTP for now. (We can add other protocols later.)
+})
 
 
