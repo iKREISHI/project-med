a74eced60b17b30e761da39fdb07abe2052d5732
shiro-kreishi <vadas25@yandex.ru>
2025-03-20 17:02:18 +0500

    added reception time endpoint

--- backend/src/api/v0/urls.py
+++ backend/src/api/v0/urls.py
@@ -16,6 +16,7 @@ from .views.registry import (
     MedicalCardViewSet
 )
 from .views.staffing.position import PositionViewSet
+from .views.staffing.reception_time import ReceptionTimeViewSet
 
 router = DefaultRouter()
 router.register('login', LoginViewSet, basename='login')
@@ -27,6 +28,7 @@ router.register('medical-card', MedicalCardViewSet, basename='medical-card')
 router.register('register-new-employee', RegistrationViewSet, basename='register-new-employee')
 router.register(r'appointments', DoctorAppointmentViewSet, basename='doctorappointment')
 router.register(r'position', PositionViewSet, basename='position')
+router.register(r'reception-time', ReceptionTimeViewSet, basename='reception-time')
 
 urlpatterns = (
 [
