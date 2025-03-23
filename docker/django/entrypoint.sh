5ccc52152f0e0c209f361766f99a9062b0010ed3
iKREISHI <vadas25@yandex.ru>
2025-03-23 18:39:16 +0500

    added nginx

--- docker/django/entrypoint.sh
+++ docker/django/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export DJANGO_SETTINGS_MODULE=config.settings
 echo "Применение миграций Django..."
+python3 /app/manage.py collectstatic --noinput --clear
 python3 /app/manage.py makemigrations users clients company_structure staffing  medical_activity registry chat
 python3 /app/manage.py migrate
 python3 /app/entrypoint.py
