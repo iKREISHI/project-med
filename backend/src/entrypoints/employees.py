7e9149fc1263d9e657dc45259c9f1c5e4c35b829
iKREISHI <vadas25@yandex.ru>
2025-03-29 01:22:08 +0500

    added some fields to filial model

--- backend/src/entrypoints/employees.py
+++ backend/src/entrypoints/employees.py
@@ -3,11 +3,9 @@ from apps.users.services.generate_username_by_fio import generate_username_by_fi
 
 
 def create_users(last_name: str, first_name: str, patronymic: str | None) -> None:
-    for i in range(1, 51):
-        username = generate_username_by_fio(last_name, first_name, patronymic)
-        password = "password123"  # можно заменить на другой пароль или сгенерировать случайный
-        user = User(username=username)
-        user.set_password(password)  # хэширование пароля
-        user.save()
-        print(f"Создан пользователь: {username}")
-
+    username = generate_username_by_fio(last_name, first_name, patronymic)
+    password = "password123"  # можно заменить на другой пароль или сгенерировать случайный
+    user = User(username=username)
+    user.set_password(password)  # хэширование пароля
+    user.save()
+    print(f"Создан пользователь: {username}")
