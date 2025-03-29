f70b7c1c8a9665bc7717df608411992cce2a1561
iKREISHI <vadas25@yandex.ru>
2025-03-29 22:44:15 +0500

    added permissions view endpoint

--- backend/src/api/v0/urls.py
+++ backend/src/api/v0/urls.py
@@ -30,6 +30,7 @@ from .views.medical_activity.hospital_stays import HospitalStaysViewSet
 from api.v0.views.medical_activity.diagnosis import DiagnosisViewSet
 from api.v0.views.medical_activity.diagnosis_category import DiagnosisCategoryViewSet
 from api.v0.views.medical_activity.booking_appointment import BookingAppointmentViewSet
+from api.v0.views.auth.permission_group import GetGroupAndPermissions4CurrentUser
 
 router = DefaultRouter()
 router.register('login', LoginViewSet, basename='login')
@@ -56,6 +57,7 @@ router.register(r'hospital-stays', HospitalStaysViewSet, basename='hospitalstays
 router.register(r'diagnoses', DiagnosisViewSet, basename='diagnosis')
 router.register(r'diagnosis-categories', DiagnosisCategoryViewSet, basename='diagnosiscategory')
 router.register(r'booking-appointments', BookingAppointmentViewSet, basename='bookingappointment')
+router.register(r'permission-group-current-user', GetGroupAndPermissions4CurrentUser, basename='permission-group-current-user')
 
 
 urlpatterns = (
