5b3068dda46281c4f42aeed4fcd01a79dd2f0496
iKREISHI <vadas25@yandex.ru>
2025-03-31 22:12:53 +0500

    added celery docker containers

---
+++ backend/src/config/celery_app.py
@@ -0,0 +1,16 @@
+import os
+from celery import Celery
+from django.conf import settings
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+app = Celery('config')
+app.config_from_object(settings.CELERY)
+app.autodiscover_tasks()
+app.conf.broker_connection_retry_on_startup = True
+# app.conf.beat_schedule = {}
+app.conf.beat_schedule = {
+    'empty-task-every-10-seconds': {
+        'task': 'apps.external_systems.tasks.empty_task',  # убедитесь, что указан правильный путь к задаче
+        'schedule': 10.0,  # интервал выполнения задачи в секундах
+    },
+}
