eed4d5083aac7a3acdfa9e17a8a963498d2a551d
iKREISHI <vadas25@yandex.ru>
2025-03-31 22:26:13 +0500

    added update medicine and update laboratory every 1 day

--- backend/src/config/celery_app.py
+++ backend/src/config/celery_app.py
@@ -3,6 +3,11 @@ from celery import Celery
 from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+from apps.external_systems.tasks import (
+    update_medicine, update_laboratory
+)
+
 app = Celery('config')
 app.config_from_object(settings.CELERY)
 app.autodiscover_tasks()
@@ -12,5 +17,13 @@ app.conf.beat_schedule = {
     'empty-task-every-10-seconds': {
         'task': 'apps.external_systems.tasks.empty_task',  # убедитесь, что указан правильный путь к задаче
         'schedule': 10.0,  # интервал выполнения задачи в секундах
+    },
+    'update-medicine-1-day': {
+        'task': 'apps.external_systems.tasks.update_medicine',
+        'schedule': 86400,
+    },
+    'update-laboratory-1-day': {
+        'task': 'apps.external_systems.tasks.update_laboratory',
+        'schedule': 86401,
     },
 }
