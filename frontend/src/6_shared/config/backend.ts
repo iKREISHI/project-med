024b3ffdc2347939b028d2f18ef2552d9dd5cf93
iKREISHI <vadas25@yandex.ru>
2025-04-02 18:38:05 +0500

    1

--- frontend/src/6_shared/config/backend.ts
+++ frontend/src/6_shared/config/backend.ts
@@ -1,3 +1,5 @@
 // @ts-nocheck
-export const backendBaseUrl = 'http://176.108.248.247/'
-export const websocketBaseUrl = '176.108.248.247'
+// export const backendBaseUrl = 'http://176.108.248.247/'
+// export const websocketBaseUrl = '176.108.248.247'
+export const backendBaseUrl = 'http://127.0.0.1/'
+export const websocketBaseUrl = '127.0.0.1'
